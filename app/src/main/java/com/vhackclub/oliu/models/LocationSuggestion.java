7c7d5f5fde27135aa62a6cc5ec7bbb9a3256bf3e
Dan Nguyen <geruk@geruk-mbp1.dhcp.thefacebook.com>
2015-07-26 06:05:12 -0700

    added location suggestion model

---
+++ app/src/main/java/com/vhackclub/oliu/models/LocationSuggestion.java
@@ -0,0 +1,15 @@
+package com.vhackclub.oliu.models;
+
+import com.parse.ParseClassName;
+import com.vhackclub.oliu.base.BaseLocation;
+import com.vhackclub.oliu.base.BaseSuggestion;
+
+/**
+ * Created by geruk on 7/26/15.
+ */
+@ParseClassName("LocationSuggestion")
+public class LocationSuggestion extends BaseSuggestion {
+    public void setLocation(BaseLocation location) {
+        put("location", location);
+    }
+}
