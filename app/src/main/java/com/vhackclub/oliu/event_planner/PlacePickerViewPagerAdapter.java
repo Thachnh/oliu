36c70c83c55eddb27f9739dde0f947d7a09d665b
Dan Nguyen <geruk@geruk-mbp1.local>
2015-07-25 22:28:56 -0700

    added a page to add option

--- app/src/main/java/com/vhackclub/oliu/event_planner/PlacePickerViewPagerAdapter.java
+++ app/src/main/java/com/vhackclub/oliu/event_planner/PlacePickerViewPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.vhackclub.oliu.event_planner;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.vhackclub.oliu.R;
+import com.vhackclub.oliu.SearchSuggestionActivity;
 import com.vhackclub.oliu.base.Suggestion;
 
 import java.util.List;
@@ -33,7 +35,7 @@ public class PlacePickerViewPagerAdapter extends PagerAdapter {
 
     @Override
     public int getCount() {
-        return mSuggestion == null ? 0 : mSuggestion.size();
+        return mSuggestion == null ? 0 : mSuggestion.size() + 1 /* Adding option */;
     }
 
     @Override
@@ -43,10 +45,21 @@ public class PlacePickerViewPagerAdapter extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        View view = mLayoutInflater.inflate(R.layout.place_card, container, false);
-        TextView title = (TextView) view.findViewById(R.id.title);
-        title.setText(mSuggestion.get(position).getName());
+        if (position < mSuggestion.size()) {
+            View view = mLayoutInflater.inflate(R.layout.place_card, container, false);
+            TextView title = (TextView) view.findViewById(R.id.title);
+            title.setText(mSuggestion.get(position).getName());
+            container.addView(view);
+            return view;
+        }
+        View view = mLayoutInflater.inflate(R.layout.add_option_page, container, false);
         container.addView(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mContext.startActivity(new Intent(mContext, SearchSuggestionActivity.class));
+            }
+        });
         return view;
     }
 
